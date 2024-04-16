f39b858e7eedc0cccea541bd19a9d4d2334621f2
Jorge <newdarkjg@gmail.com>
2024-04-16 13:38:28 +0200

    Ejercicio Acabado

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,16 @@
-const Home = () => {
+import { Link } from "react-router-dom";
+
+const Home = ({data}) => {
   return (
     <>
-    
+    <ul>
+      {data.map(item=>(
+        <li key={item._id}>
+          <Link to={`/${item._id}`}>{item.title}</Link>
+
+        </li>
+      ))}
+    </ul>
     </>
   )
 };
